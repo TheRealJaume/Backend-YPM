3bd145a8546eed727b728dfdfc9c2ccfd988e638
jaume.fabregat.work <jaume.fabregat.work@gmail.com>
2024-10-18 12:52:07 +0200

    Start and end date in project

--- ypm/project/projects/models.py
+++ ypm/project/projects/models.py
@@ -8,6 +8,8 @@ class Project(UUIDModel, SoftDeletableModel, TimeStampedModel):
     name = models.CharField(max_length=240, null=False, blank=False)
     description = models.TextField(blank=True, null=True)
     company = models.ForeignKey('company.Company', on_delete=models.CASCADE, related_name='projects')
+    init_date = models.DateTimeField(blank=True, null=True)
+    end_date = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return self.name
