e22a6d9e9982c78b47c3ea846effda1aac1724df
jaume.fabregat.work <jaume.fabregat.work@gmail.com>
2024-10-27 20:50:37 +0100

    JiraUserStore - DONE

--- ypm/users/serializers.py
+++ ypm/users/serializers.py
@@ -1,6 +1,7 @@
 import logging
 from rest_framework import serializers
 
+from users.jira.models import JiraUser
 from users.models import User
 
 logger = logging.getLogger(__name__)
@@ -16,6 +17,21 @@ class UserRetrieveSerializer(serializers.ModelSerializer):
 
 # Me (personal information)
 class UserMeRetrieveSerializer(serializers.ModelSerializer):
+
+    jira = serializers.SerializerMethodField("get_jira_user_info")
+
     class Meta:
         model = User
-        fields = ["id", "username", "email", "first_name", "last_name", "onboarding", "companies"]
+        fields = ["id", "username", "email", "first_name", "last_name", "onboarding", "companies", "jira"]
+
+    def get_jira_user_info(self, user):
+        try:
+            jira_user = JiraUser.objects.get(user=user)
+            return {
+                "username": jira_user.username,
+                "url": jira_user.url,
+                "token": jira_user.token,
+            }
+        except Exception as e:
+            logger.error(f"Error retrieving JIRA user info: {e}")
+            return None
