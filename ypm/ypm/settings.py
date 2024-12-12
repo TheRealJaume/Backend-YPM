6e4a61ec24e3003144de1df3aebbd2b1b4844ae8
TheMainProgrammer <goviralwithai@gmail.com>
2024-12-12 18:31:53 +0100

    change settings

--- ypm/ypm/settings.py
+++ ypm/ypm/settings.py
@@ -116,7 +116,7 @@ CORS_ALLOWED_ORIGINS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
