f57097ab4d842014a329834dff6ffb586a6a5bea
TheMainProgrammer <goviralwithai@gmail.com>
2024-12-19 13:16:43 +0100

    sentry config

--- ypm/ypm/urls.py
+++ ypm/ypm/urls.py
@@ -9,22 +9,29 @@ from users.urls import urlpatterns as user_urls
 from technologies.urls import urlpatterns as technology_urls
 from django.conf.urls.static import static
 
+
+# TEST SENTRY CONFIG
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # auth and token
-    path('accounts/', include('allauth.urls')),
-    path('login/google', google_login, name='google_login'),
-    path('logout', google_logout, name='google_logout'),
-    path('token/refresh/', refresh_token, name='refresh_token'),
-    path('onboarding/', onboarding, name='onboarding'),
-    # company
-    path('', include(company_urls), name='company'),
-    # project
-    path('', include(project_urls), name='project'),
-    # user
-    path('', include(user_urls), name='user'),
-    # technology
-    path('', include(technology_urls), name='technology'),
-    # payments
-    path('', include(payment_urls), name='payment'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  # auth and token
+                  path('accounts/', include('allauth.urls')),
+                  path('login/google', google_login, name='google_login'),
+                  path('logout', google_logout, name='google_logout'),
+                  path('token/refresh/', refresh_token, name='refresh_token'),
+                  path('onboarding/', onboarding, name='onboarding'),
+                  # company
+                  path('', include(company_urls), name='company'),
+                  # project
+                  path('', include(project_urls), name='project'),
+                  # user
+                  path('', include(user_urls), name='user'),
+                  # technology
+                  path('', include(technology_urls), name='technology'),
+                  # payments
+                  path('', include(payment_urls), name='payment'),
+                  path('sentry-debug/', trigger_error),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
